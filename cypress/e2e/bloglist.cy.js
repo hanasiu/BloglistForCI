69a5e95cdf9f9484e1d08ccb22740b9f6e372624
kimyu <kimyu@hanasiu.local>
2023-07-09 16:15:33 +0900

    upload

---
+++ cypress/e2e/bloglist.cy.js
@@ -0,0 +1,23 @@
+describe('template spec', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:3003')
+        cy.contains('login').click()
+        cy.get('#username').type('testid')
+        cy.get('#password').type('testid')
+        cy.get('#login-button').click()
+        cy.contains('testid logged in')
+    })
+})
+
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('http://localhost:3003')
+//     cy.contains('blogs').click()
+//     cy.contains('create new').click()
+//     const date = new Date();
+//     cy.get('#title-input').type(date.toString())
+//     cy.get('#author-input').type('jane austen')
+//     cy.get('#url-input').type('janeausten.com')
+//     cy.get('#create-button').click()
+//   })
+// })
